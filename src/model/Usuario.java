2a1b82d0dd33c2224e531e4e121b29b52a5e3756
Pedro Gouveia <pedrohenriquecombr14@gmail.com>
2026-04-12 22:14:01 -0300

    Criando classe Usuario

---
+++ src/model/Usuario.java
@@ -0,0 +1,54 @@
+package model;
+
+public class Usuario {
+    private int id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String endereco;
+
+    public Usuario(int id, String nome, String email, String telefone, String endereco) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+}
